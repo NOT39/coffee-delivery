51ce02f3dfd5ccb7b18ba03257b86f31dd4b55eb
Not <110069021+NOT39@users.noreply.github.com>
2023-03-20 23:26:56 -0300

    Style: Add coffee menu style

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,21 @@ import { styled } from '../../styles'
 
 export const HomeContainer = styled('main', {})
 
-export const MenuSection = styled('section', {})
+export const MenuSection = styled('section', {
+  width: '100%',
+  maxWidth: '70rem',
+  margin: 'auto',
 
-export const CoffeeListContainer = styled('div', {})
+  '> h1': {
+    margin: '3.5rem 0',
+    textAlign: 'left',
+  },
+})
+
+export const CoffeeListContainer = styled('div', {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(4, 1fr)',
+  gap: '2.5rem 2rem',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
